571e1e53e4abaf205e4a2791a07d8a96fb65f577
Jochem <joaghim.beltman@gmail.com>
2022-08-30 23:24:00 +0200

    Add spiral galaxy generation
    Only supported basic eliptic generation before

--- Empires/Source/Empires/BlackHole.cpp
+++ Empires/Source/Empires/BlackHole.cpp
@@ -50,17 +50,53 @@ const int ABlackHole::GetStarNumber() const
 //spawn spiral shape, on planets spawn regular elipse shape
 void ABlackHole::GenerateOrbiters(int a_Number, float a_Size)	//could add a_MinDist to put at 0.3
 {
-	//UE_LOG(LogTemp, Warning, TEXT("Called Generate Orbiters on sub"));
 	const FRotator rot = GetActorRotation();
+
+	const int numArms = 5;
+	const float armSeparationDist = 2 * PI / numArms;
+	const float armOffsetMax = 0.5f;
+	const float rotFactor = 0.00003;
+
+	int adjustedStars = 0;
 
 	for (int i = 0; i < a_Number; i++)
 	{
-		float dist = FMath::FRandRange(a_Size * m_MinDist, a_Size);
-		float angle = FMath::FRandRange(0.0f, 2 * PI);
+		// 0-1 domain
+		float dist = FMath::FRand();
+		dist = FMath::Pow(dist, 1.1);
+		float angle = FMath::FRand() * 2 * PI;
+		float armOffset = FMath::FRand() * armOffsetMax;
+		armOffset = armOffset - armOffsetMax / 2;
+		armOffset = armOffset * (1 / dist);
+
+		// transform to galaxy size
+		dist = dist * a_Size;
+		if (dist < (a_Size * m_MinDist))
+		{
+			//dist *= a_Size * m_MinDist;
+			//dist *= 10; //can do 2
+			dist += (a_Size * m_MinDist);
+			adjustedStars++;
+		}
+
+
+		float squaredArmOffset = FMath::Pow(armOffset, 2);
+		if (armOffset < 0)
+			squaredArmOffset *= -1;
+		armOffset = squaredArmOffset;
+
+		float rotation = dist * rotFactor;
+
+		angle = (int)(angle / armSeparationDist) * armSeparationDist + armOffset + rotation;
+		//angle = (int)(angle / (armSeparationDist * dist)) * (armSeparationDist * dist) + (armOffset * dist);
+		
+		
+		//angle = (int)(angle / armSeparationDist) * armSeparationDist + armOffset + rotation;
+
 
 		FVector loc;
-		loc.X = (FMath::Cos(angle) * dist) + i * 0.2f;
-		loc.Y = (FMath::Sin(angle) * dist) + i * 0.2f;
+		loc.X = (FMath::Cos(angle) * dist);
+		loc.Y = (FMath::Sin(angle) * dist);
 		loc.Z = GetActorLocation().Z;	// silly vertical Z axis
 		AActor* star = GetWorld()->SpawnActor<AActor>(m_CelestialObj, loc, rot);
 
@@ -69,5 +105,8 @@ void ABlackHole::GenerateOrbiters(int a_Number, float a_Size)	//could add a_MinD
 		m_Stars.Emplace(Cast<AStar>(star));
 		Cast<AStar>(star)->m_StarIndex = i;
 	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Manually adjusted: %d stars"), adjustedStars);
+
 	return;
 }
